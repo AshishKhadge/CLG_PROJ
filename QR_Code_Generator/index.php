5ea1c1bfeef4ac910ca06170f5a2e17daac9c58d
AshishKhadge <khadgeashish@gmail.com>
2024-03-24 19:36:26 +0530

    updated code

--- QR_Code_Generator/index.php
+++ QR_Code_Generator/index.php
@@ -1,4 +1,11 @@
+<?php
+    session_start();
 
+    if(!isset($_SESSION['loggedin']) || ($_SESSION['loggedin']) != true){
+        header("location: ../Project/login.php");
+        exit;
+    }
+?>
 <!DOCTYPE html>
 <html lang="en">
 
